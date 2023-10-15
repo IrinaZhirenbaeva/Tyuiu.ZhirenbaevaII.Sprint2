bb4258b254da464c54bca0902efb85f7de91f6d8
KK <KK@DESKTOP-TIELPRS>
2023-10-15 23:09:46 +0500

    Task1

---
+++ Tyuiu.ZhirenbaevaII.Sprint2.Task1.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZhirenbaevaII.Sprint2.Task1.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZhirenbaevaII.Sprint2.Task1.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d8f19075-c23f-465e-948a-10e0ae88e05d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ZhirenbaevaII.Sprint2.Task1.V20/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ZhirenbaevaII.Sprint2.Task1.V20.Lib;
+
+namespace Tyuiu.ZhirenbaevaII.Sprint2.Task1.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            int a = 242;
+            int b= 155;
+            int c = 456;
+            int d = 17;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+
+            Console.Title = "Спринт #2 | Выполнила: Жиренбаева И.И | ИСТНб-23-1";
+
+            Console.WriteLine("");
+            Console.WriteLine(" Спринт #2                                                               ");
+            Console.WriteLine(" Тема: Алгоритмы разветвляющейся структуры                                      ");
+            Console.WriteLine(" Задание #1                                                              ");
+            Console.WriteLine(" Вариант #20                                                            ");
+            Console.WriteLine(" Выполнила: Жиренбаева Ирина Ильгизовна | ИСТНб-23-1                       ");
+            Console.WriteLine("");
+            Console.WriteLine("* УСЛОВИЕ:                                                                ");
+            Console.WriteLine(" Написать программу из операций сравнений            ");
+            Console.WriteLine("                                         ");
+            Console.WriteLine("                                                                         ");
+            Console.WriteLine("");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        ");
+            Console.WriteLine("");
+            Console.WriteLine("a= " + a);
+            Console.WriteLine("b= " + b);
+            Console.WriteLine("c= " + c);
+            Console.WriteLine("d= " + d);
+            Console.WriteLine("**");
+            Console.WriteLine("РЕЗУЛЬТАТ:                                                               ");
+            Console.WriteLine("**");
+
+            for (int i = 0; i < 6; i++)
+
+                Console.WriteLine(res[i]);
+
+
+            Console.ReadKey();
+        }
+    }
+}
+    
+
